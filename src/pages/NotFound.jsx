531d242ef57e287ca64a7ec88dd9839da4eb4d3f
flaviare1s <flaviarster@gmail.com>
2024-09-06 07:35:21 -0300

    Ajusta estilos

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import notFound from "../assets/error404.webp";
 export const NotFound = () => {
   return (
     <section className="flex flex-col items-center justify-center m-auto h-screen">
-      <h1 className="text-4xl">Page Not Found!!!</h1>
+      <h1 className="text-2xl md:text-5xl">Page Not Found!!!</h1>
       <img className="w-[150px] mt-5" src={notFound} alt="Not Found" />
     </section>
   )
