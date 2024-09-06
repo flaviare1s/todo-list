2bf03f07f6fe836897d9f6e34d7acc33a9b66de7
flaviare1s <flaviarster@gmail.com>
2024-09-06 07:34:30 -0300

    Adiciona página de Not Found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,10 @@
+import notFound from "../assets/error404.webp";
+
 export const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <section className="flex flex-col items-center justify-center m-auto h-screen">
+      <h1 className="text-4xl">Page Not Found!!!</h1>
+      <img className="w-[150px] mt-5" src={notFound} alt="Not Found" />
+    </section>
   )
 }
