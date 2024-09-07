bda218d1b6ee862967a6139a81df6e6e56b49f4b
flaviare1s <flaviarster@gmail.com>
2024-09-07 17:35:43 -0300

    Sinaliza as todos compartilhadas

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -277,40 +277,49 @@ export const Todos = () => {
         ) : todos.length > 0 ? (
           <div className="flex flex-col border-2 border-offwhite rounded mx-auto md:w-[40%]">
             {todos.map((todo) => (
-              <div className="flex justify-between p-3 border-b" key={todo.id}>
-                {isEditing === todo.id ? (
-                  <input
-                    type="text"
-                    value={editTitle}
-                    onChange={(e) => setEditTitle(e.target.value)}
-                    onKeyDown={(e) => handleKeyDown(e, todo.id)}
-                    className="p-2 rounded focus:border-none-sm bg-inherit w-full"
-                    ref={editInputRef}
-                  />
-                ) : (
-                  <p
-                    onClick={() => changeStatus(todo.id, todo.status)}
-                    className={`text-left cursor-pointer ${
-                      todo.status === "completed"
-                        ? "line-through text-gray-500"
-                        : ""
-                    }`}
-                  >
-                    {todo.title}
-                  </p>
-                )}
-                <div className="flex gap-2">
-                  {isEditing !== todo.id && (
-                    <button onClick={() => startEditing(todo)}>
-                      <span className="material-symbols-outlined">edit</span>
-                    </button>
+              <div className="p-3 border-b" key={todo.id}>
+                <div className="flex justify-between">
+                  {isEditing === todo.id ? (
+                    <input
+                      type="text"
+                      value={editTitle}
+                      onChange={(e) => setEditTitle(e.target.value)}
+                      onKeyDown={(e) => handleKeyDown(e, todo.id)}
+                      className="p-2 rounded focus:border-none-sm bg-inherit w-full"
+                      ref={editInputRef}
+                    />
+                  ) : (
+                    <p
+                      onClick={() => changeStatus(todo.id, todo.status)}
+                      className={`text-left cursor-pointer ${
+                        todo.status === "completed"
+                          ? "line-through text-gray-500"
+                          : ""
+                      }`}
+                    >
+                      {todo.title}
+                    </p>
                   )}
-                  <button onClick={() => shareTodo(todo.id)}>
-                    <span className="material-symbols-outlined">share</span>
-                  </button>
-                  <button onClick={() => removeTodo(todo.id)}>
-                    <span className="material-symbols-outlined">close</span>
-                  </button>
+                  <div className="flex gap-2">
+                    {isEditing !== todo.id && (
+                      <button onClick={() => startEditing(todo)}>
+                        <span className="material-symbols-outlined">edit</span>
+                      </button>
+                    )}
+                    <button onClick={() => shareTodo(todo.id)}>
+                      <span className="material-symbols-outlined">share</span>
+                    </button>
+                    <button onClick={() => removeTodo(todo.id)}>
+                      <span className="material-symbols-outlined">close</span>
+                    </button>
+                  </div>
+                </div>
+                <div className="flex justify-end">
+                  {todo.sharedWith.length > 0 && (
+                    <small className="text-gray-500 mt-2">
+                      Shared
+                    </small>
+                  )}
                 </div>
               </div>
             ))}
